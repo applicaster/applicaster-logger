ac59eaad13cddc6aee3648e40a835e0deb78e5ca
Vitaly Gorodetsky <gorodetsky@gmail.com>
2015-06-03 08:31:49 +0300

    fixes uninitialized constant Sidekiq::Job

--- lib/applicaster/logger.rb
+++ lib/applicaster/logger.rb
@@ -35,6 +35,8 @@ module Applicaster
       end
 
       if defined?(::Sidekiq)
+        require 'sidekiq/api'
+
         ::Sidekiq.configure_server do |config|
           config.server_middleware do |chain|
             chain.remove ::Sidekiq::Middleware::Server::Logging
