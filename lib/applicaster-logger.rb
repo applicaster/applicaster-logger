0e59765d2485d2a70430e7782a51aa8e7561b650
Neer Friedman <neerfri@gmail.com>
2018-08-10 14:37:45 +0300

    support sidekiq 5 (#16)

--- lib/applicaster-logger.rb
+++ lib/applicaster-logger.rb
@@ -1,3 +1,5 @@
 require 'applicaster/logger'
 require "applicaster/rack/request_data"
 require "applicaster/sidekiq/middleware"
+require "applicaster/sidekiq/job_logger"
+require "applicaster/sidekiq/exception_logger"
