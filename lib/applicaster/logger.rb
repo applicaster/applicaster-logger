a895e9275e81267e1ca1a59aa5b5157cda51a247
Neer Friedman <neerfri@gmail.com>
2018-09-15 23:29:07 +0300

    move methods related to Railtie into its file

--- lib/applicaster/logger.rb
+++ lib/applicaster/logger.rb
@@ -7,29 +7,6 @@ module Applicaster
     # taken from https://github.com/rails/rails/blob/master/actionpack/lib/action_controller/log_subscriber.rb
     INTERNAL_PARAMS = %w(controller action format only_path)
 
-    def self.setup_lograge(app)
-      app.config.lograge.enabled = true
-      app.config.lograge.formatter = Lograge::Formatters::Logstash.new
-      app.config.lograge.custom_options = lambda do |event|
-        {
-          params: event.payload[:params].except(*INTERNAL_PARAMS).inspect,
-          facility: "action_controller",
-          custom_params: event.payload[:custom_params],
-        }
-      end
-
-      app.middleware.insert_after ActionDispatch::RequestId,
-        Applicaster::Rack::RequestData
-    end
-
-    def self.setup_logger(app)
-      logstash_config = app.config.applicaster_logger.logstash_config
-      app.config.logger = new_logger("rails_logger")
-      Applicaster::Logger::Sidekiq.setup(new_logger("sidekiq")) if defined?(::Sidekiq)
-      Sidetiq.logger = new_logger("sidetiq") if defined?(Sidetiq)
-      Delayed::Worker.logger = new_logger("sidekiq") if defined?(Delayed)
-    end
-
     def self.with_thread_data(data)
       old, Thread.current[:logger_thread_data] =
         Thread.current[:logger_thread_data], data
@@ -72,14 +49,6 @@ module Applicaster
 
           cut << omission
         end
-      end
-    end
-
-    def self.new_logger(facility)
-      config = ::Rails.application.config.applicaster_logger
-      LogStashLogger.new(config.logstash_config).tap do |logger|
-        logger.level = config.applicaster_logger.level
-        logger.formatter = Applicaster::Logger::Formatter.new(facility: "rails_logger")
       end
     end
   end
--- lib/applicaster/logger/railtie.rb
+++ lib/applicaster/logger/railtie.rb
@@ -12,11 +12,43 @@ module Applicaster
       config.applicaster_logger.application_name = Rails.application.class.parent.to_s.underscore
 
       initializer :applicaster_logger_lograge, before: :lograge do |app|
-        Applicaster::Logger.setup_lograge(app) if app.config.applicaster_logger.enabled
+        setup_lograge(app) if app.config.applicaster_logger.enabled
       end
 
       initializer :applicaster_logger, before: :initialize_logger do |app|
-        Applicaster::Logger.setup_logger(app) if app.config.applicaster_logger.enabled
+        setup_logger(app) if app.config.applicaster_logger.enabled
+      end
+
+      def setup_lograge(app)
+        app.config.lograge.enabled = true
+        app.config.lograge.formatter = Lograge::Formatters::Logstash.new
+        app.config.lograge.custom_options = lambda do |event|
+          {
+            params: event.payload[:params].except(*INTERNAL_PARAMS).inspect,
+            facility: "action_controller",
+            custom_params: event.payload[:custom_params],
+          }
+        end
+
+        app.middleware.insert_after ActionDispatch::RequestId,
+          Applicaster::Rack::RequestData
+      end
+
+      def setup_logger(app)
+        config = app.config.applicaster_logger
+        app.config.logger = new_logger("rails_logger")
+        Applicaster::Logger::Sidekiq.setup(new_logger("sidekiq")) if defined?(::Sidekiq)
+        Sidetiq.logger = new_logger("sidetiq") if defined?(Sidetiq)
+        Delayed::Worker.logger = new_logger("delayed") if defined?(Delayed)
+      end
+
+      def new_logger(facility)
+        config = ::Rails.application.config.applicaster_logger
+        LogStashLogger.new(config.logstash_config).tap do |logger|
+          puts "new logger for #{facility}: #{logger}"
+          logger.level = config.level
+          logger.formatter = Applicaster::Logger::Formatter.new(facility: facility)
+        end
       end
     end
   end
